87c6c1bb6d3f303d8084d5165469db18cafbd07b
Laly Azzopardi <laly.azzopardi@ens.uvsq.fr>
2023-05-21 13:31:17 +0200

    Q8

--- laly code.py
+++ laly code.py
@@ -43,7 +43,7 @@ def Cr(mat):
     return MatCr
 
 def YCbCr(mat):
-    MatYCbCr = np.empty(mat.shape)
+    MatYCbCr = np.empty((mat.shape[0], mat.shape[1], 3))
     for i in range(mat.shape[0]):
         for j in range(mat.shape[1]):
             R = 0.299 * mat[i, j, 0] + 0.587 * mat[i, j, 1] + 0.114 * mat[i, j, 2]
@@ -62,6 +62,18 @@ def RGB2(mat):
             MatRGB[i, j] = (np.uint8(np.clip(R, 0.0, 255.0)), np.uint8(np.clip(G, 0.0, 255.0)), np.uint8(np.clip(B, 0.0, 255.0)))
     return MatRGB
 
+test = load("test.png")
+
+# Conversion de l'image en YCbCr et RGB
+test_yCbCr = YCbCr(test)
+test_RGB = RGB2(test_yCbCr)
+
+test = load("test.png")
+Image.fromarray(test,'RGB').show()
+Image.fromarray(test_yCbCr,'YCbCr').show()
+Image.fromarray(test_RGB,'RGB').show()
+
+
 def add_padding(image, pad_size):
     if isinstance(pad_size, int):
         pad_size = (pad_size, pad_size)  # Si un seul entier est donné, le même padding sera ajouté dans les deux dimensions
@@ -79,7 +91,6 @@ def remove_padding(padded_image, pad_size):
 
     return image
 
-# Fonction Matrice sous-echantillonnee 
 #question 4
 def matrice_sousechantillon(mat):
     matrice_se = mat[::2, ::2]
@@ -107,6 +118,57 @@ def get_block(mat):
 matrice = np.array([[1,2,5,6,0,0,0,0],[3,4,7,8,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]])
 print(get_block(matrice))
 
+#question7
+def transform_frequence(liste_de_bloc):
+    liste_final=[]
+    for i in liste_de_bloc :
+        liste_final.append(dct2(i))
+    liste_final=np.array(liste_final)
+    return liste_final
+
+
+def detransform_frequence(liste_de_bloc):
+    liste_final=[]
+    for i in liste_de_bloc :
+        liste_final.append(idct2(i))
+    liste_final=np.array(liste_final)
+    return liste_final
+
+matrice = np.array([[1,2,5,6,0,0,0,0],[3,4,7,8,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]])
+listb = get_block(matrice)
+print(listb)
+listt = transform_frequence(listb)
+print(listt)
+listdt = transform_frequence(listt)
+print(listdt)
+
+#Question 8 
+def compress_mode0(image):
+    yCbCr_image = YCbCr(image)
+    padded_image = add_padding(yCbCr_image)
+    blocks = get_block(padded_image)
+
+    return blocks
+
+
+def compress_mode1(image, seuil):
+    yCbCr_image = YCbCr(image)
+    padded_image = add_padding(yCbCr_image)
+    blocks = get_block(padded_image)
+    blocks[np.abs(blocks) < seuil] = 0
+
+    return blocks
+
+
+def compress_mode2(image, seuil):
+    yCbCr_image = YCbCr(image)
+    padded_image = add_padding(yCbCr_image)
+    blocks = get_block(padded_image)
+    blocks[np.abs(blocks) < seuil] = 0
+    subsampled_Cb = matrice_sousechantillon(Cb(padded_image))
+    subsampled_Cr = matrice_sousechantillon(Cr(padded_image))
+
+    return blocks, subsampled_Cb, subsampled_Cr
 
 # Charger l'image
 image = load("test.png")
@@ -123,14 +185,6 @@ subsampled_RGB = matrice_sousechantillon(RGB2(padded_image))
 padded_RGB = RGB2(padded_image)
 restored_RGB = remove_padding(padded_RGB, pad_size)
 
-
-test = load("test.png")
-# Afficher l'image convertie en YCbCr
-Image.fromarray(restored_yCbCr.astype(np.uint8), 'YCbCr').show()
-
-# Afficher l'image convertie en RGB
-Image.fromarray(restored_RGB.astype(np.uint8), 'RGB').show()
-
 # Charger l'image
 image = Image.open("test.png")
 image_array = np.array(image)
@@ -145,4 +199,19 @@ doubled_image = matrice_2d(subsampled_image)
 Image.fromarray(image_array).show()
 Image.fromarray(subsampled_image).show()
 Image.fromarray(doubled_image).show()
+
+#Compression
+# Charger l'image
+image = load("test.png")
+
+# Mode 0 : blocs transformés tels quels
+compressed_mode0 = compress_mode0(image)
+
+# Mode 1 : seuil appliqué aux coefficients
+seuil = 8
+compressed_mode1 = compress_mode1(image, seuil)
+
+# Mode 2 : seuil appliqué aux coefficients + sous-échantillonnage de la chrominance
+seuil = 8
+compressed_mode2, subsampled_Cb, subsampled_Cr = compress_mode2(image, seuil)
 
